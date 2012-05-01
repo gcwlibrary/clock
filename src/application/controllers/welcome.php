3cfa1f20143b66280b07765f7a17b0ed310ed652
Jamie Mahoney <jmahoney127@googlemail.com>
2012-05-01 11:02:57 +0100

    Demo controller
    Will still need changing, but is good as a starting point.

--- src/application/controllers/welcome.php
+++ src/application/controllers/welcome.php
@@ -1,27 +1,60 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+/**
+* Welcome // Filename
+*
+* PHP Version 5
+* 
+* @category  Badges
+* @package   Badges
+* @author    Jamie Mahoney <jmahoney@lincoln.ac.uk>
+* @license   GNU Affero General Public License 3.0
+* @link      coursedata.blogs.lincoln.ac.uk
+*
+* Copyright University of Lincoln 2012
+*
+* This file is part of Badges.
+*
+* Badges is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Affero Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Badges is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with Badges.  If not, see <http://www.gnu.org/licenses/>. 
+*
+*/
+
+/**
+* Welcomes // Class name
+*
+* @category Badges
+* @package  Badges
+* @author   Jamie Mahoney <jmahoney@lincoln.ac.uk>
+* @license  GNU Affero General Public License 3.0
+* @link     coursedata.blogs.lincoln.ac.uk
+*
+*/
 
 class Welcome extends CI_Controller {
 
 	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -  
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in 
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
-	 */
+	* Brief description of the function
+	*
+	* @param type $variable_name brief description
+	*
+	* @return Nothing / Whatever it returns 
+	* @access Public
+	*/
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		//Do stuff
 	}
 }
 
-/* End of file welcome.php */
-/* Location: ./application/controllers/welcome.php */
+// End of file welcome.php 
+// Location: ./controllers/welcome.php
